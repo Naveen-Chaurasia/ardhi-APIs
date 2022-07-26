a8078965cec40296d98cdb4b13be6a12db1c7148
Naveen <naveen.chaurasia861@gmail.com>
2022-07-26 15:24:51 +0530

    filtered entities and repos

--- Ardhi-carbonCafe/src/main/java/com/ardhi/neo4jAccess/repositories/Level1Repo.java
+++ Ardhi-carbonCafe/src/main/java/com/ardhi/neo4jAccess/repositories/Level1Repo.java
@@ -4,8 +4,6 @@ package com.ardhi.neo4jAccess.repositories;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import com.ardhi.neo4jAccess.Entities.Category;
 import com.ardhi.neo4jAccess.Entities.Level1;
 
 @RepositoryRestResource(collectionResourceRel = "Level1", path = "Level1")
--- Ardhi-carbonCafe/src/main/java/com/ardhi/neo4jAccess/services/service.java
+++ Ardhi-carbonCafe/src/main/java/com/ardhi/neo4jAccess/services/service.java
@@ -7,32 +7,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ardhi.neo4jAccess.Entities.Category;
-import com.ardhi.neo4jAccess.Entities.ProductComponents;
-import com.ardhi.neo4jAccess.Entities.Products;
-import com.ardhi.neo4jAccess.repositories.CategoryRepo;
 import com.ardhi.neo4jAccess.repositories.Level0Repo;
 import com.ardhi.neo4jAccess.repositories.Level1Repo;
-import com.ardhi.neo4jAccess.repositories.ProductComponentsRepo;
-import com.ardhi.neo4jAccess.repositories.ProductRepo;
 
 @Service
 public class service {
 
-	@Autowired
-	CategoryRepo cr;
-	@Autowired
-	ProductRepo pr;
-	@Autowired
-	ProductComponentsRepo pcr;
 	@Autowired
 	Level0Repo l0r;
 	
 	@Autowired
 	Level1Repo l1r;
 	
-	
-//	
 //	public List<Products> getProducts()
 //	{
 //		return pr.findAll();
