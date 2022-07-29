3723e06b0428c7b1ea04de5c703531c8bfb8d9a7
Naveen <naveen.chaurasia861@gmail.com>
2022-07-29 11:56:28 +0530

    added junit Testcases

--- Ardhi-carbonCafe/src/test/java/com/ardhi/neo4jAccess/Neo4jAccessApplicationTests.java
+++ Ardhi-carbonCafe/src/test/java/com/ardhi/neo4jAccess/Neo4jAccessApplicationTests.java
@@ -1,13 +1,42 @@
 package com.ardhi.neo4jAccess;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.ardhi.neo4jAccess.Entities.Level1;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 @SpringBootTest
 class Neo4jAccessApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
+	 @Autowired
+	    private WebApplicationContext wac;
+
+	    private MockMvc mvc;
+	
+	 @Test
+	   public void getProductsList() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/0";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
 
 }
