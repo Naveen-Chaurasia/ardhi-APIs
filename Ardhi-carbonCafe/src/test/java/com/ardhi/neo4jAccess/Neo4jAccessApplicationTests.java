d22702c4cd54ac7763e9f2655c473411ee94ff36
Naveen <naveen.chaurasia861@gmail.com>
2022-07-29 12:12:41 +0530

    added junit Testcases

--- Ardhi-carbonCafe/src/test/java/com/ardhi/neo4jAccess/Neo4jAccessApplicationTests.java
+++ Ardhi-carbonCafe/src/test/java/com/ardhi/neo4jAccess/Neo4jAccessApplicationTests.java
@@ -27,16 +27,72 @@ class Neo4jAccessApplicationTests {
 	    private MockMvc mvc;
 	
 	 @Test
-	   public void getProductsList() throws Exception {
+	   public void getLevel1CFRP() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/30";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
+	 @Test
+	   public void getLevel1Steel() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/40";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
+	 @Test
+	   public void getLevel1GFRP() throws Exception {
 		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
 	        this.mvc = builder.build();
 	      String uri = "/Level1/0";
 	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-	      
 	      int status = mvcResult.getResponse().getStatus();
 	      assertEquals(200, status);
 
 	   }
+	 @Test
+	   public void getLevel2CFRP() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/30/Level2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
+	 @Test
+	   public void getLevel2GFRP() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/0/Level2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
+	 @Test
+	   public void getLevel2Steel() throws Exception {
+		 DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+	        this.mvc = builder.build();
+	      String uri = "/Level1/40/Level2";
+	      MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	         .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	      int status = mvcResult.getResponse().getStatus();
+	      assertEquals(200, status);
+
+	   }
+	 
+	 
 
 }
